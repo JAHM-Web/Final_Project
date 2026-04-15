350ee50c604f3a2ab137eba17e075c238a24f9bf
Anika Balakrishnan <balakr24@msu.edu>
2026-04-15 18:51:41 +0000

    map

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
 import { useEffect, useState } from 'react';
+import 'leaflet/dist/leaflet.css';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+
 
 function App() {
   const [posts, setPosts] = useState([]);
@@ -13,6 +16,17 @@ function App() {
   return (
     <div className="App">
   <header className="App-header">
+    <MapContainer
+        center={[42.73, -84.48]}  // East Lansing coords
+        zoom={13}
+        style={{ height: "400px", width: "600px" }}
+      >
+        <TileLayer
+          attribution='&copy; OpenStreetMap contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+      </MapContainer>
+
     <h1>Posts from Rails API</h1>
     {posts.length === 0 ? (
       <p>Loading...</p>
